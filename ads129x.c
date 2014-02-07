22ce736e37b39d110b053ae52062a0b8927ca652
Auryn Verwegen <auryn.verwegen@topic.nl>
2014-02-07 15:33:43 +0100

    Refactoring to normal SPI Protocol driver

--- ads129x.c
+++ ads129x.c
@@ -17,150 +17,206 @@
 #include <linux/delay.h>
 #include <linux/module.h>
 #include <linux/interrupt.h>
+#include <linux/irq.h>
+#include <linux/gpio.h>
+#include <linux/of_gpio.h>
 
-#include <linux/iio/iio.h>
-#include <linux/iio/sysfs.h>
-#include <linux/iio/buffer.h>
-#include <linux/iio/trigger_consumer.h>
-#include <linux/iio/triggered_buffer.h>
+#include <linux/fs.h>
+#include <linux/cdev.h>
 
 #include <linux/of_device.h>
 #include <linux/of_address.h>
+
+
+struct ads_cdev{
+	struct class *cl;
+	dev_t dev_no;
+	struct cdev dev;
+};
+
+struct ads129x_dev {
+	struct ads_cdev cdev;
+};
+
+static struct ads129x_dev ads_inst;
 
 struct ads129x_state {
 	struct spi_device *spi;
 };
 
-#define ADS129x_V_CHAN(index)						\
-	{								\
-		.type = IIO_VOLTAGE,					\
-		.indexed = 1,						\
-		.channel = index,					\
-		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),		\
-		.info_mask_shared_by_type = BIT(IIO_CHAN_INFO_SCALE),	\
-		.address = index,					\
-		.scan_index = index,					\
-		.scan_type = {						\
-			.sign = 's',					\
-			.realbits = 24,					\
-			.storagebits = 24,				\
-			.endianness = IIO_BE,				\
-		},							\
-	}
+struct gpio ads129x_gpio_clksel = { .label = "clksel-gpio", .flags = GPIOF_OUT_INIT_LOW };
+struct gpio ads129x_gpio_drdy = { .label = "drdy-gpio", .flags = GPIOF_IN };
+struct gpio ads129x_gpio_pwdn = { .label = "pwdn-gpio", .flags = GPIOF_OUT_INIT_LOW };
+struct gpio ads129x_gpio_reset = { .label = "reset-gpio", .flags = GPIOF_OUT_INIT_LOW };
+struct gpio ads129x_gpio_start = { .label = "start-gpio", .flags =GPIOF_OUT_INIT_LOW };
 
 
-static const struct iio_chan_spec ads129x_channels[] = {
-	{
-		.type = IIO_PROXIMITY, /* Actually, status bits for channels */
-		.indexed = 1,
-		.channel = 0,
-		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
-		.address = -1,
-		.scan_index = -1,
-		.scan_type = {
-			.sign = 'u',
-			.realbits = 24,
-			.storagebits = 24,
-		},
-	},
-	ADS129x_V_CHAN(0),
-	ADS129x_V_CHAN(1),
-	ADS129x_V_CHAN(2),
-	ADS129x_V_CHAN(3),
-	ADS129x_V_CHAN(4),
-	ADS129x_V_CHAN(5),
-	ADS129x_V_CHAN(6),
-	ADS129x_V_CHAN(7),
-};
-
-
-/**
- * ads129x_update_scan_mode() setup the spi transfer buffer for the new scan mask
- **/
-static int ads129x_update_scan_mode(struct iio_dev *indio_dev,
-	const unsigned long *active_scan_mask)
+static irqreturn_t ads129x_irq_handler(int irq, void *id)
 {
-	struct ads129x_state *st = iio_priv(indio_dev);
-
-	pr_debug("%s\n", __func__);
-
-	return -ENODEV;
-}
-
-/**
- * ads129x_trigger_handler() bh of trigger launched polling to ring buffer
- **/
-static irqreturn_t ads129x_trigger_handler(int irq, void *p)
-{
-	struct iio_poll_func *pf = p;
-	struct iio_dev *indio_dev = pf->indio_dev;
-	struct ads129x_state *st = iio_priv(indio_dev);
-
-	pr_debug("%s\n", __func__);
-
+	gpio_set_value(ads129x_gpio_clksel.gpio, gpio_get_value(ads129x_gpio_clksel.gpio) ? 0 : 1 );
+        
 	return IRQ_HANDLED;
 }
 
-static int ads129x_read_raw(struct iio_dev *indio_dev,
-			   struct iio_chan_spec const *chan,
-			   int *val,
-			   int *val2,
-			   long m)
-{
-	struct ads129x_state *st = iio_priv(indio_dev);
-	pr_debug("%s (%d)\n", __func__, m);
-	return -ENODEV;
-}
 
-static const struct iio_info ads129x_info = {
-	.read_raw = &ads129x_read_raw,
-	.update_scan_mode = ads129x_update_scan_mode,
-	.driver_module = THIS_MODULE,
+static int ads129x_init_io_from_dt(struct device *dev, struct gpio *pgpio){
+	int ret;
+	int irq;
+	struct device_node *np = dev->of_node;
+	
+	pgpio->gpio = of_get_named_gpio(np, pgpio->label, 0);
+	printk("%s: %d\n", pgpio->label, pgpio->gpio);
+	if(!gpio_is_valid(pgpio->gpio)){
+		dev_err(dev, "Invalid gpio\n");
+		return -EINVAL;
+	}else{
+		ret = devm_gpio_request_one(dev, pgpio->gpio, pgpio->flags, pgpio->label);
+		if(ret < 0){
+			printk("Error requesting gpio!\n");
+			return ret;
+		}
+		if(pgpio->flags == GPIOF_IN){
+			irq = gpio_to_irq(pgpio->gpio);
+			printk(" - %s-int = %d\n", pgpio->label, irq);
+			devm_request_irq(dev, irq, ads129x_irq_handler, 0, "ads192x irq handler", NULL);
+			irq_set_irq_type(irq, IRQ_TYPE_EDGE_RISING);
+		}
+	}
+
+	return 0;
+};
+
+static int ads129x_init_gpio_pins(struct device *dev){
+	int ret;
+
+	ret = ads129x_init_io_from_dt(dev, &ads129x_gpio_clksel);
+	if(ret < 0){ return ret; }
+	ret = ads129x_init_io_from_dt(dev, &ads129x_gpio_drdy);
+	if(ret < 0){ return ret; }
+	ret = ads129x_init_io_from_dt(dev, &ads129x_gpio_pwdn); 
+	if(ret < 0){ return ret; }
+	ret = ads129x_init_io_from_dt(dev, &ads129x_gpio_reset);
+	if(ret < 0){ return ret; }
+	ret = ads129x_init_io_from_dt(dev, &ads129x_gpio_start);
+	if(ret < 0){ return ret; }
+
+	return 0;
+};
+
+static loff_t ads_cdev_llseek(struct file *filp, loff_t off, int whence){
+	printk(" # llseek\n");
+	return 0;
+};
+
+static ssize_t ads_cdev_read(struct file *filp, char __user *buf, size_t count, loff_t *f_pos){
+        printk(" # read\n");
+	return 0;
+};
+
+static ssize_t ads_cdev_write(struct file *filp, const char __user *buf, size_t count, loff_t *pos){
+        printk("# write\n");
+	return 0;
+};
+
+static long ads_cdev_ioctl(struct file *filp, unsigned int cmd, unsigned long arg){
+        printk(" # ioctl\n");
+	return 0;
+};
+
+static int ads_cdev_open(struct inode *inode, struct file *filp){
+        printk(" # open\n");
+	return 0;
+};
+
+static int ads_cdev_release(struct inode *inode, struct file *filp){
+        printk(" # release\n");
+	return 0;
+};
+
+
+
+static const struct file_operations fops = {
+        .owner = THIS_MODULE,
+        .llseek = ads_cdev_llseek,
+        .read = ads_cdev_read,
+        .write = ads_cdev_write,
+        .unlocked_ioctl = ads_cdev_ioctl,
+        .open = ads_cdev_open,
+        .release = ads_cdev_release,
+};
+
+
+static int ads129x_init_cdev(struct ads129x_dev *ads){
+	int ret;
+
+	ret = alloc_chrdev_region( &ads->cdev.dev_no /*&devt*/ , 0, 1,"ads129x_cdev");
+	if (ret < 0) {
+		printk("alloc_chrdev_region failed!\n");
+		goto error_exit; 
+	}
+	ads->cdev.cl = class_create(THIS_MODULE, "ads129x_cdev");
+	if(ads->cdev.cl == NULL){
+		printk("Could not create class!\n");
+		goto cleanup_region;
+	}
+	
+	if(device_create(ads->cdev.cl, NULL, ads->cdev.dev_no, NULL, "ads129x_cdev") == NULL){
+		printk("Could not create character device!\n");
+		goto cleanup_class;
+	}
+
+	cdev_init(&ads->cdev.dev, &fops );
+	ret = cdev_add(&ads->cdev.dev, ads->cdev.dev_no, 1);
+	if(ret < 0){
+		printk("Could not add character device!\n");
+		goto cleanup_dev;
+	}
+
+	return 0;
+
+cleanup_dev:
+	device_destroy(ads->cdev.cl, ads->cdev.dev_no);
+cleanup_class:
+	class_destroy(ads->cdev.cl);
+cleanup_region:
+	unregister_chrdev_region(ads->cdev.dev_no, 1);
+error_exit:
+	return (ret < 0) ? ret : -1;
 };
 
 static int ads129x_probe(struct spi_device *spi)
 {
-	struct ads129x_state *st;
-	struct iio_dev *indio_dev;
+//	struct ads129x_state *st;
 	int ret;
 
-	indio_dev = devm_iio_device_alloc(&spi->dev, sizeof(*st));
-	if (indio_dev == NULL)
-		return -ENOMEM;
-	st = iio_priv(indio_dev);
-	spi_set_drvdata(spi, indio_dev);
-	st->spi = spi;
+	printk("-- ads129x probe(..)!\n");
+	
+	ret = ads129x_init_gpio_pins(&spi->dev);
 
-	indio_dev->name = spi_get_device_id(spi)->name;
-	indio_dev->dev.parent = &spi->dev;
-	indio_dev->modes = INDIO_DIRECT_MODE;
-	indio_dev->channels = ads129x_channels;
-	indio_dev->num_channels = ARRAY_SIZE(ads129x_channels);
-	indio_dev->info = &ads129x_info;
+	//if(ret < 0){ TODO }
 
-	/* Setup default message */
-	ret = iio_triggered_buffer_setup(indio_dev, NULL,
-			&ads129x_trigger_handler, NULL);
-	if (ret)
-		goto error_disable_reg;
+	ret = ads129x_init_cdev(&ads_inst);
+	if(ret < 0){
+		return ret;
+	}
 
-	ret = iio_device_register(indio_dev);
-	if (ret)
-		goto error_cleanup_ring;
+	printk("Writing test data... ( ");
+	//ret = spi_write(spi, "Spi Test Data", 13);
 
-	return 0;
+	printk("%d )\n", ret);
 
-error_cleanup_ring:
-error_disable_reg:
 	return ret;
 }
 
 static int ads129x_remove(struct spi_device *spi)
 {
-	struct iio_dev *indio_dev = spi_get_drvdata(spi);
-	struct ads129x_state *st = iio_priv(indio_dev);
+	cdev_del(&ads_inst.cdev.dev);
+	device_destroy(ads_inst.cdev.cl, ads_inst.cdev.dev_no);
+	class_destroy(ads_inst.cdev.cl);
 
-	iio_device_unregister(indio_dev);
+	unregister_chrdev_region(ads_inst.cdev.dev_no, 1);
+
+	// TODO: Cleanup
 
 	return 0;
 }
